4c14fb2cc0742a5bc7f4a6c2f9980ce1ee3af755
Mr.halomoan n <oansmaturapax123@gmail.com>
2022-05-02 21:07:27 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.menu');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+    menu.classList.toggle('move');
+    bell.classList.remove('active');
+}
+
+// Notification
+let bell = document.querySelector('.notification');
+document.querySelector('#bell-icon').onclick = () => {
+    bell.classList.toggle('active');
+}
+
+// Swiper
+var swiper = new Swiper(".trending-content", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1068: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+    },
+  });
